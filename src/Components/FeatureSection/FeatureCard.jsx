e165b88af607a5967e3a9011653137e3eb1661fb
Terka Codes <107133029+terka-codes@users.noreply.github.com>
2023-05-09 21:16:28 +0200

    wip on features cards

--- src/Components/FeatureSection/FeatureCard.jsx
+++ src/Components/FeatureSection/FeatureCard.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import "./FeatureCard.css"
 
 function FeatureCard(props) {
     return (
-        <div>
-            <img src={props.logo} alt={props.heading + "image"} />
-            <h3>{props.heading}</h3>
-            <p>{props.text}</p>
+        <div className="feature__card">
+            <div className="feature__card__image-container">
+                <img className="feature__card__image" src={props.logo} alt={props.heading + "image"} />
+            </div>
+            <h3 className="feature__card__heading">{props.heading}</h3>
+            <p className="feature__card__text">{props.text}</p>
         </div>
     )
 }
--- src/Components/FeatureSection/FeatureCards.jsx
+++ src/Components/FeatureSection/FeatureCards.jsx
@@ -2,12 +2,24 @@ import React from 'react'
 import FeatureCard from "./FeatureCard"
 import "./FeatureCards.css"
 import brandRecognitionSvg from "../../assets/images/icon-brand-recognition.svg"
+import detailedRecordsSvg from "../../assets/images/icon-detailed-records.svg"
+import fullyCustomizableSvg from "../../assets/images/icon-fully-customizable.svg"
 
 const cards = [
     {
         logo: brandRecognitionSvg,
         heading: "Brand Recognition",
         text: "Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.",
+    },
+    {
+        logo: detailedRecordsSvg,
+        heading: "Detailed Records",
+        text: "Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.",
+    },
+    {
+        logo: fullyCustomizableSvg,
+        heading: "Fully Customizable",
+        text: "Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.",
     }
 ]
 
