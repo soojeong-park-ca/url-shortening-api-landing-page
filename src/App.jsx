e18bc5aa967bf975843e52f441227700773eec76
Soojeong Park <soojeong.park.ca@gmail.com>
2023-05-09 11:16:25 -0700

    add hero mobile version

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header/Header";
 import Footer from "./Components/Footer/Footer";
+import Hero from "./components/Hero/Hero";
 import "./App.css";
 import HeaderBlankHeight from "./components/Header/HeaderBlankHeight";
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Header />
+      <Hero />
       <HeaderBlankHeight />
       <Footer />
     </>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,13 @@
-const Button = (props) => {
-    return (
-        <button className="bg-[#2acfcf] text-white font-[Poppins] py-2 px-6 rounded-full md:ml-8 hover:bg-[#209f9f] font-bold text-sm duration-500">
-            {props.children}
-        </button>
-    )
-}
+const Button = props => {
+  return (
+    <button
+      className={`bg-[#2acfcf] text-white font-[Poppins] py-2 px-6 rounded-full md:ml-8 hover:bg-[#209f9f] font-bold text-sm duration-500 ${
+        props.className || ""
+      }`}
+    >
+      {props.children}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
