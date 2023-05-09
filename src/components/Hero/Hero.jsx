addbbd92b434e6817ef7e3466fe172e617931639
Soojeong Park <soojeong.park.ca@gmail.com>
2023-05-09 14:42:06 -0700

    fix background image sizing and spacing

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,16 +5,17 @@ import "./Hero.css";
 export default function Hero() {
   return (
     <section id="hero" className="hero">
-      <div className="heroBgImgBox"></div>
-      <div className="heroText">
-        <h1 className="headingPrimary">More than just shorter links</h1>
-        <p className="paragraph">
-          Build your brand's recognition and get detailed insights on how your
-          links are performing .
-        </p>
-        <Button className="btnHero">Get Started</Button>
+      <div className="heroBox">
+        <div className="heroBgImgBox"></div>
+        <div className="heroText">
+          <h1 className="headingPrimary">More than just shorter links</h1>
+          <p className="paragraph">
+            Build your brand's recognition and get detailed insights on how your
+            links are performing .
+          </p>
+          <Button className="btnHero">Get Started</Button>
+        </div>
       </div>
-      <div></div>
     </section>
   );
 }
