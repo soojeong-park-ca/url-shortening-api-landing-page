b2e0433b52fd382a34f5e82b55c83084a4dfd3d7
Terka Codes <107133029+terka-codes@users.noreply.github.com>
2023-05-16 21:46:39 +0200

    Feature section finished w/o the URL app section

--- src/Components/FeatureSection/FeatureCards.jsx
+++ src/Components/FeatureSection/FeatureCards.jsx
@@ -13,13 +13,13 @@ const cards = [
         text: "Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.",
     },
     {
-        id: 4,
+        id: 1,
         logo: detailedRecordsSvg,
         heading: "Detailed Records",
         text: "Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.",
     },
     {
-        id: 6,
+        id: 2,
         logo: fullyCustomizableSvg,
         heading: "Fully Customizable",
         text: "Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.",
---
+++ src/Components/FeatureSection/UrlShotenerApp/UrlShortenerApp.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./UrlShortenerApp.css"
+
+/* This is just a markup so i could finish the rest */
+
+function UrlShortenerApp() {
+    return (
+        <div className="url">
+            <form>
+                <input type="text" placeholder="fghjkl" />
+                <button type="submit">Shorten It!</button>
+            </form>
+        </div>
+    )
+}
+
+export default UrlShortenerApp
